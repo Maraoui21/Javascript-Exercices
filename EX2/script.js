9b4fba8a0f73d0b4d7086353428eb7a9027be79d
Maraoui21 <y.maraoui@etu.enset-media.ac.ma>
2022-03-23 21:38:22 +0100

    second modif

--- EX2/script.js
+++ EX2/script.js
@@ -39,7 +39,7 @@ for(i=0;i<2;i++){
         })
     }else{
         tab.forEach(element => {
-            template = `<td><img src="${element.img}" height="${element.value}" width="50px"/></td>`;
+            template += `<td><img src="${element.img}" height="${element.value}" width="50px"/></td>`;
         });
     }
     template+= `</tr>`;
