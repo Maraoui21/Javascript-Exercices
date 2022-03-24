5a9628bc22849ce0d1409be959f4f498dbf2f8e1
Maraoui21 <y.maraoui@etu.enset-media.ac.ma>
2022-03-24 09:01:59 +0100

    modif N4

---
+++ EX4/script.js
@@ -0,0 +1,11 @@
+
+function charRandom(){
+    
+    let chaineCar = "DAR";
+    const size = chaineCar.length - 1;
+    let i=size;
+    while(i>=0){
+        console.log("hey");
+        i--;
+    }
+}
